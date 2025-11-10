17f8c6208d06eb024a155b922383c846d5b33827
Serhii Ivashchenko <Ivashchenkoserezha25@gmail.com>
2025-11-10 20:06:01 +0100

    add second part

---
+++ js/hw-4/task-3.js
@@ -0,0 +1,22 @@
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+profile.changeUsername("Ivan");
+console.log(profile.getInfo()); // "Ivan has 320 active hours!"
