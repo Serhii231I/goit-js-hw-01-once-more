4cfdc88043df10c8140b869089a7dfd5d2c1c0ad
Serhii Ivashchenko <Ivashchenkoserezha25@gmail.com>
2025-11-15 17:48:27 +0100

    HW5

--- js/hw-4/task-1.js
+++ js/hw-4/task-1.js
@@ -1,3 +1,15 @@
+// const numbers = [5, 10, 15, 20, 25];
+
+// // Класичний for
+// for (let i = 0; i < numbers.length; i += 1) {
+//   console.log(`Index ${i}, value ${numbers[i]}`);
+// }
+
+// // Перебираючий метод forEach
+// numbers.forEach(function (number, index) {
+//   console.log(`Index ${index}, value ${number}`);
+// });
+
 function isEnoughCapacity(products, containerSize) {
   let total = 0;
   const cap = Object.values(products);
